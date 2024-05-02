4ff3891c1ce40e8c4afc5a1cea00e9b37ea2c4bf
Garrett HE <garrett.he@outlook.com>
2024-05-02 11:42:51 +0800

    Add plugin entry for d2loader

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,6 +1,16 @@
 #include <Windows.h>
+#include <d2loader/plugin.h>
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
     return TRUE;
+}
+
+DWORD __stdcall PluginEntry(DWORD dwReason, LPVOID pData) {
+    return TRUE;
+}
+
+LPPLUGIN_INTERFACE __stdcall QueryInterface() {
+    static PLUGIN_INTERFACE Interface = { PLUGIN_MAGICWORD, PLUGIN_VERSION, "D2LUA", PluginEntry };
+    return &Interface;
 }
